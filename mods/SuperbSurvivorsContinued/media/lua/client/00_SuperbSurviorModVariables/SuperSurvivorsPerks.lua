22fb46fea6dca7c1763db5021f9348166de9957b
Maze-SSR <cnbsumi@yahoo.com.au>
2025-05-02 01:55:00 +1000

    Expanded SurvivorPerks list for B42 compatibility and future features
    Reorganized and expanded SurvivorPerks with B42-compatible perk names
    
    Replaced deprecated PlantScavenging with Foraging
    
    Added FirstAid alongside legacy Doctor for medical role support
    
    Included new perks: Woodwork, Bartering, Leadership for crafting and future social systems
    
    Structured by category for better readability and easier role assignment

--- mods/SuperbSurvivorsContinued/media/lua/client/00_SuperbSurviorModVariables/SuperSurvivorsPerks.lua
+++ mods/SuperbSurvivorsContinued/media/lua/client/00_SuperbSurviorModVariables/SuperSurvivorsPerks.lua
@@ -1,23 +1,45 @@
+-- SuperSurvivorPerks.lua
+-- Global list of Perks considered for survivor roles and behaviors
 
 SurvivorPerks = {
-	"Aiming",
-	"Axe",
-	"Combat",
-	"SmallBlade",
-	"LongBlade",
-	"SmallBlunt",
-	"Blunt",
-	"Maintenance",
-	"Spear",
-	"Doctor",
-	"Farming",
-	"Firearm",
-	"Reloading",
-	"Fitness",
-	"Lightfoot",
-	"Nimble",
-	"PlantScavenging",
-	"Sneak",
-	"Strength",
-	"Survivalist"
+    -- Combat Skills
+    "Aiming",
+    "Reloading",
+    "Axe",
+    "SmallBlade",
+    "LongBlade",
+    "SmallBlunt",
+    "Blunt",
+    "Spear",
+    "Maintenance",
+
+    -- Physical Skills
+    "Strength",
+    "Fitness",
+    "Lightfoot",
+    "Nimble",
+    "Sneak",
+
+    -- Survival & Scavenging
+    "Foraging",             -- Renamed from PlantScavenging in B42
+    "Trapping",
+    "Fishing",
+    "Survivalist",
+
+    -- Crafting & Construction
+    "Woodwork",             -- Carpentry
+    "Cooking",
+    "Tailoring",
+    "MetalWelding",
+    "Mechanics",
+    "Electricity",
+
+    -- Agriculture & Medical
+    "Farming",
+    "FirstAid",             -- B42-compatible version of "Doctor"
+    "Doctor",               -- Keep legacy support if used elsewhere
+
+    -- Social / Utility (for future dialogue or relationship systems)
+    "Bartering",
+    "Leadership"
 }
