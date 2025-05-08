a83a4e5c12bd2445eb6e1c43dc423966e3b49cb7
Maze-SSR <cnbsumi@yahoo.com.au>
2025-05-08 14:21:34 +1000

    Cleaned up GoCheckItOutTask logic and removed redundant condition
    - Removed unused condition block in isComplete() that served no purpose.
    - Added optional debug log line for easier tracing if logging is enabled.
    - Ensured function structure is cleaner and logically consistent.
    - This minor cleanup will make behavior easier to debug during testing.
    
    This change does not alter functionality, but helps ensure more predictable behavior in task flow.

--- mods/SuperbSurvivorsContinued/media/lua/client/05_Tasks/GoCheckItOutTask.lua
+++ mods/SuperbSurvivorsContinued/media/lua/client/05_Tasks/GoCheckItOutTask.lua
@@ -1,53 +1,55 @@
 GoCheckItOutTask = {}
 GoCheckItOutTask.__index = GoCheckItOutTask
 
+local isLocalLoggingEnabled = false;
+
 function GoCheckItOutTask:new(superSurvivor, spot)
-	local o = {}
-	setmetatable(o, self)
-	self.__index = self
+    CreateLogLine("GoCheckItOutTask", isLocalLoggingEnabled, "function: GoCheckItOutTask:new() called");
 
-	o.parent = superSurvivor
-	o.Name = "Check out spot"
-	o.Spot = spot
-	o.SeenSpot = false
-	o.Ticks = 0
-	o.Complete = false
-	o.parent:Speak(Get_SS_Dialogue("WhatWasThatSound"))
+    local o = {}
+    setmetatable(o, self)
+    self.__index = self
 
-	return o
+    o.parent = superSurvivor
+    o.Name = "Check out spot"
+    o.Spot = spot
+    o.SeenSpot = false
+    o.Ticks = 0
+    o.Complete = false
+    o.parent:Speak(Get_SS_Dialogue("WhatWasThatSound"))
+
+    return o
 end
 
 function GoCheckItOutTask:isComplete()
-	if (self.Complete) then
-
-	end
-	return self.Complete
+    return self.Complete
 end
 
 function GoCheckItOutTask:isValid()
-	if not self.parent or self.Spot == nil then
-		return false
-	else
-		return true
-	end
+    if not self.parent or self.Spot == nil then
+        return false
+    else
+        return true
+    end
 end
 
 function GoCheckItOutTask:update()
-	if (not self:isValid()) then
-		self.Complete = true
-		return false
-	end
+    if (not self:isValid()) then
+        self.Complete = true
+        return false
+    end
 
-	if (self.parent:isInAction() == false) then
-		local distance = GetDistanceBetween(self.parent:Get(), self.Spot)
+    if (self.parent:isInAction() == false) then
+        local player = self.parent:Get()
+        local distance = GetDistanceBetween(player, self.Spot)
 
-		if (distance <= 2.0) then
-			self.Complete = true
-		else
-			self.parent:WalkToPoint(self.Spot:getX(), self.Spot:getY(), self.Spot:getZ())
-		end
+        if (distance <= 2.0) then
+            self.Complete = true
+        else
+            self.parent:WalkToPoint(self.Spot:getX(), self.Spot:getY(), self.Spot:getZ())
+        end
 
-		self.Ticks = self.Ticks + 1
-		if (self.Ticks > 15) then self.Complete = true end
-	end
+        self.Ticks = self.Ticks + 1
+        if (self.Ticks > 15) then self.Complete = true end
+    end
 end
